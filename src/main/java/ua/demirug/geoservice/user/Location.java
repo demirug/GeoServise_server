c145ce62cceadffcf8899d2bc01cdb74d9427b3a
demirug <pm.antondolia@gmail.com>
2022-02-27 13:47:46 +0200

    Split User to User/Location classes, moved to user folder

---
+++ src/main/java/ua/demirug/geoservice/user/Location.java
@@ -0,0 +1,9 @@
+package ua.demirug.geoservice.user;
+
+import lombok.Value;
+
+@Value
+public class Location {
+    float x;
+    float y;
+}
---
+++ src/main/java/ua/demirug/geoservice/user/User.java
@@ -0,0 +1,25 @@
+package ua.demirug.geoservice.user;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Value
+@Builder
+public class User {
+
+    UUID uuid;
+
+    @Getter(AccessLevel.NONE)
+    Date last_request;
+
+    Location location;
+
+    UUID[] target;
+
+    public String  getLast_request() {
+        return this.last_request.toString();
+    }
+
+}
