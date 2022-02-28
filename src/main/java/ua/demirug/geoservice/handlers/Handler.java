ff5ec9e0cccbcbe259bfb1cd34c1acff87c1770c
demirug <pm.antondolia@gmail.com>
2022-03-01 01:20:47 +0200

    Serialize/Sending back data not works in run_handler

--- src/main/java/ua/demirug/geoservice/handlers/Handler.java
+++ src/main/java/ua/demirug/geoservice/handlers/Handler.java
@@ -1,8 +1,13 @@
 package ua.demirug.geoservice.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
+import ua.demirug.geoservice.resonses.MessageResponse;
+import ua.demirug.geoservice.resonses.Response;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -14,6 +19,8 @@ import java.util.regex.Pattern;
 import static java.util.stream.Collectors.*;
 
 public abstract class Handler {
+
+    private Jsonb jsonb = JsonbBuilder.create();
 
     public Map<String, List<String>> splitQuery(String query) {
         if (query == null || "".equals(query)) {
@@ -30,6 +37,27 @@ public abstract class Handler {
         return encoded == null ? null : URLDecoder.decode(encoded, StandardCharsets.UTF_8);
     }
 
-    public abstract void handle(HttpExchange exchange) throws IOException;
+    public void run_handle(HttpExchange exchange) throws IOException {
+
+        if (!exchange.getRequestMethod().equals("POST")) {
+            throw new UnsupportedOperationException();
+        }
+
+        System.out.println("POST " + exchange.getRequestURI());
+
+        Response response = this.handle(exchange);
+        response = response != null ? response : new MessageResponse("No response was given");
+
+        String data = this.jsonb.toJson(response);
+
+        exchange.sendResponseHeaders(200, data.getBytes().length);
+        OutputStream output = exchange.getResponseBody();
+        output.write(data.getBytes());
+        output.flush();
+
+        exchange.close();
+    }
+
+    public abstract Response handle(HttpExchange exchange);
 
 }
