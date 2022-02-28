0e256f1146948a0d3957f8cfed1eac5a7e77fd60
demirug <pm.antondolia@gmail.com>
2022-03-01 01:21:25 +0200

    Now works with new Handler class

--- src/main/java/ua/demirug/geoservice/handlers/RegistrationHandler.java
+++ src/main/java/ua/demirug/geoservice/handlers/RegistrationHandler.java
@@ -1,34 +1,25 @@
 package ua.demirug.geoservice.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
+
+import ua.demirug.geoservice.resonses.RegisterResponse;
+import ua.demirug.geoservice.resonses.Response;
 import ua.demirug.geoservice.services.UserManager;
 import ua.demirug.geoservice.user.User;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.UUID;
 
 public class RegistrationHandler extends Handler {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
 
     /* Generate user and returned User JSON */
-    public void handle(HttpExchange exchange) throws IOException {
 
-        if (!exchange.getRequestMethod().equals("POST")) {
-            throw new UnsupportedOperationException();
-        }
+    @Override
+    public Response handle(HttpExchange exchange) {
 
         User user = UserManager.registerUser(UUID.randomUUID());
 
-        String data = this.objectMapper.writeValueAsString(user);
-
-        exchange.sendResponseHeaders(200, data.getBytes().length);
-        OutputStream output = exchange.getResponseBody();
-        output.write(data.getBytes());
-        output.flush();
-
-        exchange.close();
+        return new RegisterResponse(user.getUuid().toString());
     }
+
 }
