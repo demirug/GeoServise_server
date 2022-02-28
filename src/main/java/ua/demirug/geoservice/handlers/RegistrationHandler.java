242f782bfbf0443d895d4f77fe7052e29ee8916d
demirug <pm.antondolia@gmail.com>
2022-02-28 13:10:25 +0200

    Created registration by UserManager

--- src/main/java/ua/demirug/geoservice/handlers/RegistrationHandler.java
+++ src/main/java/ua/demirug/geoservice/handlers/RegistrationHandler.java
@@ -1,27 +1,32 @@
 package ua.demirug.geoservice.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
-import ua.demirug.geoservice.User;
+import ua.demirug.geoservice.services.UserManager;
+import ua.demirug.geoservice.user.User;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
 
-public class RegistrationHandler {
+public class RegistrationHandler extends Handler {
 
-    /* Generate and returned UUID */
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    /* Generate user and returned User JSON */
     public void handle(HttpExchange exchange) throws IOException {
 
         if (!exchange.getRequestMethod().equals("POST")) {
             throw new UnsupportedOperationException();
         }
 
-        String uuid = UUID.randomUUID().toString();
-        User.builder().uuid(uuid).build();
+        User user = UserManager.registerUser(UUID.randomUUID());
 
-        exchange.sendResponseHeaders(200, uuid.getBytes().length);
+        String data = this.objectMapper.writeValueAsString(user);
+
+        exchange.sendResponseHeaders(200, data.getBytes().length);
         OutputStream output = exchange.getResponseBody();
-        output.write(uuid.getBytes());
+        output.write(data.getBytes());
         output.flush();
 
         exchange.close();
