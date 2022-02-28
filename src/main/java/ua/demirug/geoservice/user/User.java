b228a01a68de01ecd818c9bb5edf27eba3bd200e
demirug <pm.antondolia@gmail.com>
2022-02-28 15:56:07 +0200

    Removed target and custom getter

--- src/main/java/ua/demirug/geoservice/user/User.java
+++ src/main/java/ua/demirug/geoservice/user/User.java
@@ -11,15 +11,9 @@ public class User {
 
     UUID uuid;
 
-    @Getter(AccessLevel.NONE)
     Date last_request;
 
     Location location;
 
-    UUID[] target;
-
-    public String  getLast_request() {
-        return this.last_request.toString();
-    }
 
 }
