b6ef4064e4d918daeb515d32586524c6e2e679f9
demirug <pm.antondolia@gmail.com>
2022-03-01 01:25:38 +0200

    Registered get/update paths

--- src/main/java/ua/demirug/geoservice/Application.java
+++ src/main/java/ua/demirug/geoservice/Application.java
@@ -1,6 +1,8 @@
 package ua.demirug.geoservice;
 
 import com.sun.net.httpserver.HttpServer;
+import ua.demirug.geoservice.handlers.GetDataHandler;
+import ua.demirug.geoservice.handlers.UpdateDataHandler;
 import ua.demirug.geoservice.handlers.RegistrationHandler;
 import ua.demirug.geoservice.services.datebase.AccountBase;
 
@@ -11,15 +13,19 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
         AccountBase.getInstance().init();
+
         InetSocketAddress address = new InetSocketAddress(8000);
         HttpServer server = HttpServer.create(address, 0);
 
-        server.createContext("/register",  new RegistrationHandler()::handle);
+        server.createContext("/register",  new RegistrationHandler()::run_handle);
+        server.createContext("/update",  new UpdateDataHandler()::run_handle);
+        server.createContext("/get",  new GetDataHandler()::run_handle);
 
         server.setExecutor(null);
         server.start();
 
         System.out.println("Server started at " + address.getHostString() + ":" + address.getPort());
+
     }
 
 }
