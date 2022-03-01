7eaf602f7c37f18bffab0da5926651f8dc7b70b1
demirug <pm.antondolia@gmail.com>
2022-03-01 02:45:46 +0200

    Init configs, getting host/port from config

--- src/main/java/ua/demirug/geoservice/Application.java
+++ src/main/java/ua/demirug/geoservice/Application.java
@@ -1,20 +1,25 @@
 package ua.demirug.geoservice;
 
 import com.sun.net.httpserver.HttpServer;
+import ua.demirug.geoservice.config.ConfigProvider;
 import ua.demirug.geoservice.handlers.GetDataHandler;
 import ua.demirug.geoservice.handlers.UpdateDataHandler;
 import ua.demirug.geoservice.handlers.RegistrationHandler;
 import ua.demirug.geoservice.services.datebase.AccountBase;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class Application {
 
     public static void main(String[] args) throws IOException {
+        ConfigProvider.getInstance().load();
         AccountBase.getInstance().init();
+        InetAddress host = InetAddress.getByName(ConfigProvider.getInstance().getString("host"));
+        int port = ConfigProvider.getInstance().getInteger("port");
 
-        InetSocketAddress address = new InetSocketAddress(8000);
+        InetSocketAddress address = new InetSocketAddress(host, port);
         HttpServer server = HttpServer.create(address, 0);
 
         server.createContext("/register",  new RegistrationHandler()::run_handle);
